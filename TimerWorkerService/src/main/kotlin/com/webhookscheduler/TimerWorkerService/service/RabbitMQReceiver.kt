9ac71a56bc4432ea97522ad27998eccb4a07b40a
Chayanika Khatua <chayanika.khatua@gmail.com>
2024-05-16 17:27:16 +0200

    chore: remove redundant messages list in worker

--- TimerWorkerService/src/main/kotlin/com/webhookscheduler/TimerWorkerService/service/RabbitMQReceiver.kt
+++ TimerWorkerService/src/main/kotlin/com/webhookscheduler/TimerWorkerService/service/RabbitMQReceiver.kt
@@ -9,24 +9,14 @@ import org.springframework.stereotype.Service
 @Service
 class RabbitMQReceiver {
     private val objectMapper = jacksonObjectMapper()
-    private val messages = mutableListOf<String>()
 
     @RabbitListener(queues = [RabbitMQConfig.QUEUE_NAME])
     fun receive(message: String) {
         println("Received message: $message")
-        messages.add(message)
 
         val payload: TimerDTO = objectMapper.readValue(message, TimerDTO::class.java)
         println("Payload: $payload")
 
         // fire webhook
-    }
-
-    fun getMessages(): List<String> {
-        return messages.toList() // Return a copy of the messages
-    }
-
-    fun clearMessages() {
-        messages.clear()
     }
 }
